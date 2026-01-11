b8e143ae1e057a3a81cd891b1512ff91f8f8aeaa
Ansh30-har <84302841+Ansh30-har@users.noreply.github.com>
2026-01-12 02:49:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 import os
-from aiogram import Bot, Dispatcher, executor, types
 
-TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
 
-@dp.message_handler(commands=['start'])
-async def start(msg: types.Message):
-    await msg.answer("✅ Bot is running!")
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Bot is alive ✅")
+
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    asyncio.run(main())
